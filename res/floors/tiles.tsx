4493204f5bb04f605cfa55138de95ad58b80a326
dong0625 <dong0625@korea.ac.kr>
2025-08-20 18:09:10 +0900

    fix(Python) resource 저장 폴더 통일
    - resource를 저장하는 폴더 res로 통일

---
+++ res/floors/tiles.tsx
@@ -0,0 +1,156 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tiles" tilewidth="48" tileheight="48" tilecount="19" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2" x="0" y="0" width="32" height="32">
+  <properties>
+   <property name="name" value="poop"/>
+   <property name="type" value="obstacle"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/poops.png" width="160" height="160"/>
+ </tile>
+ <tile id="3" x="0" y="0" width="32" height="32">
+  <properties>
+   <property name="name" value="heart"/>
+   <property name="type" value="pickup"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/pickup_hearts.png" width="96" height="96"/>
+ </tile>
+ <tile id="4" x="0" y="32" width="32" height="32">
+  <properties>
+   <property name="name" value="heart"/>
+   <property name="type" value="pickup"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/pickup_hearts.png" width="96" height="96"/>
+ </tile>
+ <tile id="5" x="0" y="0" width="32" height="32">
+  <properties>
+   <property name="name" value="bomb"/>
+   <property name="type" value="pickup"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/bombs.png" width="160" height="192"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="item"/>
+   <property name="type" value="pickup"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/phd.png" width="32" height="32"/>
+ </tile>
+ <tile id="7" x="0" y="0" width="32" height="32">
+  <properties>
+   <property name="name" value="rock"/>
+   <property name="type" value="obstacle"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/rocks.png" width="128" height="256"/>
+ </tile>
+ <tile id="8" x="0" y="0" width="32" height="32">
+  <properties>
+   <property name="name" value="pill"/>
+   <property name="type" value="pickup"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/pills.png" width="96" height="128"/>
+ </tile>
+ <tile id="10" x="0" y="0" width="16" height="32">
+  <properties>
+   <property name="name" value="key"/>
+   <property name="type" value="pickup"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/keys.png" width="64" height="64"/>
+ </tile>
+ <tile id="11" x="0" y="0" width="48" height="48">
+  <properties>
+   <property name="name" value="fire"/>
+   <property name="type" value="obstacle"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/fire_top.png" width="384" height="96"/>
+ </tile>
+ <tile id="13" x="16" y="16" width="32" height="32">
+  <properties>
+   <property name="name" value="coin"/>
+   <property name="type" value="pickup"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/penny.png" width="544" height="192"/>
+ </tile>
+ <tile id="9" x="0" y="48" width="32" height="32">
+  <properties>
+   <property name="name" value="chest"/>
+   <property name="type" value="chest"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/chests.png" width="256" height="256"/>
+ </tile>
+ <tile id="14" x="0" y="0" width="32" height="32">
+  <properties>
+   <property name="name" value="fly"/>
+   <property name="type" value="enemy"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/enemies/fly.png" width="256" height="256"/>
+ </tile>
+ <tile id="15" x="32" y="32" width="32" height="32">
+  <properties>
+   <property name="name" value="attackFly"/>
+   <property name="type" value="enemy"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/enemies/fly.png" width="256" height="256"/>
+ </tile>
+ <tile id="16" x="0" y="0" width="32" height="32">
+  <properties>
+   <property name="name" value="pooter"/>
+   <property name="type" value="enemy"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/enemies/pooter.png" width="128" height="128"/>
+ </tile>
+ <tile id="17" x="0" y="0" width="32" height="32">
+  <properties>
+   <property name="name" value="horf"/>
+   <property name="type" value="enemy"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/enemies/Horf.png" width="64" height="64"/>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="name" value="pacer"/>
+   <property name="type" value="enemy"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/enemies/pacer_title.png" width="18" height="13"/>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="name" value="gusher"/>
+   <property name="type" value="enemy"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/enemies/gusher_title.png" width="24" height="26"/>
+ </tile>
+ <tile id="20" x="0" y="0" width="32" height="16">
+  <properties>
+   <property name="name" value="spider"/>
+   <property name="type" value="enemy"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/enemies/spider.png" width="320" height="192"/>
+ </tile>
+ <tile id="21" x="0" y="0" width="32" height="16">
+  <properties>
+   <property name="name" value="bigSpider"/>
+   <property name="type" value="enemy"/>
+   <property name="variant" value="0"/>
+  </properties>
+  <image source="../textures/enemies/bigSpider.png" width="64" height="64"/>
+ </tile>
+</tileset>
